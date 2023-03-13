8f05d69741a1c9b0775f2a12263f19154cac3abc
Alejandro Tomasella <aletomasella09@gmail.com>
2023-03-12 21:39:44 -0300

    test: Testing how tauri works

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,18 +1,17 @@
-// Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-use std::{fs::File, io::Write};
+// use std::{fs::File, io::Write};
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
-fn greet(name: &str) -> String {
-    let greeting = format!("Hello, {}! You've been greeted from Rust!", name);
-    let mut file = File::create("test.txt").unwrap();
-    file.write_all(greeting.as_bytes()).unwrap();
-    return greeting;
-}
+fn greet(name: &str) -> String {  
+    let mut greeting = format!("Hello, {}! You've been greeted from Rust!", name);
+    greeting = greeting.to_ascii_uppercase();
+     greeting
+   }
+
 
 fn main() {
     tauri::Builder::default()
         .invoke_handler(tauri::generate_handler![greet])
-        .run(tauri::generate_context!())
-        .expect("error while running tauri application");
+        .run(tauri::generate_context!()) 
+        .expect("error while running tauri application"); 
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,30 @@
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
 import { invoke } from "@tauri-apps/api/tauri";
-import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
-  async function greet() {
+  async function greet(e: React.MouseEvent<HTMLButtonElement, MouseEvent>) {
+    e.preventDefault();
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
     setGreetMsg(await invoke("greet", { name }));
   }
 
   return (
-    <div className="container">
-      <h1>Welcome to Tauri!</h1>
-
-      <div className="row">
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo vite" alt="Vite logo" />
-        </a>
-        <a href="https://tauri.app" target="_blank">
-          <img src="/tauri.svg" className="logo tauri" alt="Tauri logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <div>Hello</div>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <button onClick={(e) => greet(e)}>Greet</button>
+      <div>
+        <h1>Probando</h1>
       </div>
-
-      <p>Click on the Tauri, Vite, and React logos to learn more.</p>
-
-      <div className="row">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            greet();
-          }}
-        >
-          <input
-            id="greet-input"
-            onChange={(e) => setName(e.currentTarget.value)}
-            placeholder="Enter a name..."
-          />
-          <button type="submit">Greet</button>
-        </form>
-      </div>
-      <p>{greetMsg}</p>
-    </div>
+      {greetMsg && <div>{greetMsg}</div>}
+    </>
   );
 }
 
